9acf9dd091852bd980c3fa9d7c9425a8451119d6
narayan-nehaal <np6879@srmist.edu.in>
2026-03-26 15:31:51 +0530

    UC1 2

--- src/main/java/org/example/use_case_1.java
+++ src/main/java/org/example/use_case_1.java
@@ -3,5 +3,6 @@ package org.example;
 public class use_case_1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Welcome to GitHub Use Case 1");
     }
 }
